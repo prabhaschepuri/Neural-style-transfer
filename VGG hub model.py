2be9864e0184f03d1dfc44bcbeeb895ec4b72a24
prabhaschepuri <prabhaschepuri@gmail.com>
2026-06-03 12:58:58 +0530

    Add loading of pretrained VGG model from TensorFlow Hub
    Load pretrained VGG model for neural style transfer from TensorFlow Hub.

---
+++ VGG hub model.py
@@ -0,0 +1,5 @@
+## Loading of Pretrained VGG model for Neural style transfer from tensorflow hub
+
+hub_model = hub.load(
+    'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+)
